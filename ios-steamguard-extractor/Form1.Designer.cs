88bbbe380f2836ccc9225a9f3001f1f54fbb680f
caitsith2 <d_good@caitsith2.com>
2017-01-31 00:56:34 -0800

    Initial commit

---
+++ ios-steamguard-extractor/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ios_steamguard_extractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.btnGetSteamGuardData = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtResults
+            // 
+            this.txtResults.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtResults.Location = new System.Drawing.Point(0, 0);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResults.Size = new System.Drawing.Size(562, 469);
+            this.txtResults.TabIndex = 0;
+            // 
+            // btnGetSteamGuardData
+            // 
+            this.btnGetSteamGuardData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnGetSteamGuardData.Location = new System.Drawing.Point(0, 0);
+            this.btnGetSteamGuardData.Name = "btnGetSteamGuardData";
+            this.btnGetSteamGuardData.Size = new System.Drawing.Size(562, 35);
+            this.btnGetSteamGuardData.TabIndex = 1;
+            this.btnGetSteamGuardData.Text = "Get Steamguard Authenticator Data";
+            this.btnGetSteamGuardData.UseVisualStyleBackColor = true;
+            this.btnGetSteamGuardData.Click += new System.EventHandler(this.btnGetSteamGuardData_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtResults);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnGetSteamGuardData);
+            this.splitContainer1.Size = new System.Drawing.Size(562, 508);
+            this.splitContainer1.SplitterDistance = 469;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 508);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "ios backup SteamGuard Authenticator data extractor";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Button btnGetSteamGuardData;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ ios-steamguard-extractor/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Claunia.PropertyList;
+using Newtonsoft.Json;
+
+namespace ios_steamguard_extractor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static int SearchBytes(byte[] haystack, byte[] needle, int start)
+        {
+            var len = needle.Length;
+            var limit = haystack.Length - len;
+            for (var i = start; i <= limit; i++)
+            {
+                var k = 0;
+                for (; k < len; k++)
+                {
+                    if (needle[k] != haystack[i + k]) break;
+                }
+                if (k == len) return i;
+            }
+            return -1;
+        }
+
+        private void btnGetSteamGuardData_Click(object sender, EventArgs e)
+        {
+            var iosBackups = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                                "Apple Computer", "MobileSync", "Backup");
+            if (!Directory.Exists(iosBackups))
+            {
+                txtResults.Text = "No ios backups found";
+                return;
+            }
+            foreach (var d in Directory.GetDirectories(iosBackups))
+            {
+                var name = new DirectoryInfo(d).Name;
+                if (!File.Exists(Path.Combine(d, "Manifest.mbdb")))
+                {
+                    txtResults.AppendText($"Directory {name} is not a valid ios backup, skipping" + Environment.NewLine + Environment.NewLine);
+                    continue;
+                }
+                txtResults.AppendText($"Processing Directory {name}" + Environment.NewLine);
+                var data = File.ReadAllBytes(Path.Combine(d, "Manifest.mbdb"));
+                var steamfiles = Encoding.UTF8.GetBytes("AppDomain-com.valvesoftware.Steam");
+                for (var index = 0; ; index += steamfiles.Length)
+                {
+                    index = SearchBytes(data, steamfiles, index);
+                    if (index == -1) break;
+                    var index2 = index + steamfiles.Length;
+                    var filelen = data[index2] << 8 | data[index2 + 1];
+                    var temp = new byte[filelen];
+                    Array.Copy(data, index2 + 2, temp, 0, filelen);
+                    var steamfilename = Encoding.UTF8.GetString(temp);
+                    if (!steamfilename.StartsWith("Documents/Steamguard-")) continue;
+                    var hash =
+                        new SHA1Managed().ComputeHash(
+                            Encoding.UTF8.GetBytes("AppDomain-com.valvesoftware.Steam-" + steamfilename));
+                    var hashstr = BitConverter.ToString(hash).Replace("-", "");
+                    if (File.Exists(Path.Combine(d, hashstr)))
+                    {
+                        txtResults.AppendText($"Processing {steamfilename}" + Environment.NewLine);
+                        try
+                        {
+                            var sgdata = BinaryPropertyListParser.Parse(File.ReadAllBytes(Path.Combine(d, hashstr)));
+                            var sglist = ((NSArray) ((NSDictionary) sgdata)["$objects"]).GetArray();
+                            var auth = new SteamAuthenticator()
+                            {
+                                SharedSecret = sglist[14].ToString(),
+                                Uri = sglist[15].ToString(),
+                                Steamid = sglist[16].ToString(),
+                                RevocationCode = sglist[17].ToString(),
+                                SerialNumber = sglist[18].ToString(),
+                                TokenGid = sglist[19].ToString(),
+                                IdentitySecret = sglist[20].ToString(),
+                                Secret = sglist[21].ToString(),
+                                ServerTime = sglist[22].ToString(),
+                                AccountName = sglist[23].ToString(),
+                                SteamguardScheme = sglist[24].ToString(),
+                                Status = sglist[25].ToString()
+                            };
+                            txtResults.AppendText(Environment.NewLine);
+
+                            txtResults.AppendText("In WinAuth, Add Steam Authenticator. Select the Import Android Tab" +
+                                                  Environment.NewLine + Environment.NewLine);
+
+                            txtResults.AppendText("Paste this into the steam_uuid.xml text box" + Environment.NewLine);
+                            txtResults.AppendText($"android:{name}" + Environment.NewLine + Environment.NewLine);
+
+                            txtResults.AppendText(
+                                "Paste the following data, including the {} into the SteamGuare-NNNNNNNNN... text box" +
+                                Environment.NewLine);
+
+                            txtResults.AppendText(JsonConvert.SerializeObject(auth, Formatting.Indented) +
+                                                  Environment.NewLine + Environment.NewLine);
+                        }
+                        catch (PropertyListFormatException) //The only way this should happen is if we opened an encrypted backup.
+                        {
+                            txtResults.AppendText("Error: Encrypted backups are not supported. You need to create a decrypted backup to proceed." + Environment.NewLine);
+                            break;
+                        }
+                        catch (Exception ex)
+                        {
+                            txtResults.AppendText($"An Exception occurred while processing: {ex.Message}");
+                        }
+
+                    }
+                    else
+                    {
+                        txtResults.AppendText($"Error: {steamfilename} is missing from ios backup, aborting" + Environment.NewLine);
+                        break;
+                    }
+                }
+
+                txtResults.AppendText("Done" + Environment.NewLine + Environment.NewLine);
+            }
+        }
+    }
+
+    public class SteamAuthenticator
+    {
+        [JsonProperty("shared_secret")]
+        public string SharedSecret { get; set; }
+
+        [JsonProperty("uri")]
+        public string Uri { get; set; }
+
+        [JsonProperty("steamid")]
+        public string Steamid { get; set; }
+
+        [JsonProperty("revocation_code")]
+        public string RevocationCode { get; set; }
+
+        [JsonProperty("serial_number")]
+        public string SerialNumber { get; set; }
+
+        [JsonProperty("token_gid")]
+        public string TokenGid { get; set; }
+
+        [JsonProperty("identity_secret")]
+        public string IdentitySecret { get; set; }
+
+        [JsonProperty("secret_1")]
+        public string Secret { get; set; }
+
+        [JsonProperty("server_time")]
+        public string ServerTime { get; set; }
+
+        [JsonProperty("account_name")]
+        public string AccountName { get; set; }
+
+        [JsonProperty("steamguard_scheme")]
+        public string SteamguardScheme { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+}
+}
